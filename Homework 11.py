e345508f55e275324f543432d4a752b4fc54ec66
Prostello <167526229+Prostello@users.noreply.github.com>
2024-04-22 18:51:43 +0300

    Add files via upload
    Домашняя работа по уроку "Функции в Python.Функция с параметром

---
+++ Homework 11.py
@@ -0,0 +1,8 @@
+def print_params(soname):
+    print('Soname is:', soname)
+    print('Soname is:', soname)
+print_params('Prostakov')
+print()
+print_params('Samonom')
+print()
+print_params('Putin')
